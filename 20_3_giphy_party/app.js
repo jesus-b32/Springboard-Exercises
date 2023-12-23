2036151dc6f180125e40dc1e95e95e81480fd2ec
jesus <149453045+jesus-b32@users.noreply.github.com>
2023-12-22 21:47:37 -0800

    completed giphy party exercise

--- 20_3_giphy_party/app.js
+++ 20_3_giphy_party/app.js
@@ -1,26 +1,40 @@
-console.log("Let's get this party started!");
-
 const gifForm = document.querySelector('#gif_form');
-const searchButton = document.querySelector('#searchBtn');
 const removeButton = document.querySelector('#removeBtn');
 
-function gifSearchHandler(e) {
+// append GIF URL to DOM
+function appendGif (gifURL) {
+    const gifContainer = document.querySelector('#gif_container');
+    const img = document.createElement('img');
+    img.src = gifURL;
+    gifContainer.append(img);
+}
+
+// Use Giphy API to retrive a GIF using a search term. Also will handel any error if encountered
+async function getGifURL (searchTerm) {
+    try {
+        let randomIndex = Math.floor(Math.random()*50);
+        const result = await axios.get('https://api.giphy.com/v1/gifs/search', {params: {q: searchTerm, api_key: '7i1v2G2AELzzTnZPvjNV5I4FKYMP3hA5'}});
+        return result.data.data[randomIndex].images.fixed_height.url;
+    } catch(e) {
+        alert('There was an issue. Try again later.');
+    }
+}
+
+// get search term from user input submit, send to API, and apend the GIF recieved from API
+async function gifSearchHandler(e) {
     e.preventDefault();
     const gifSearch = document.querySelector('#searchBar');
-    let input = gifSearch.value;
-    // run aysnc function that uses user input to search for a gif
-    getGIFs(input);
+    const gif = await getGifURL(gifSearch.value); 
+    appendGif(gif);
     gifSearch.value = '';
 }
 
-
-async function getGIFs (searchTerm) {
-    console.log(searchTerm);
-    const result = await axios.get('http://api.giphy.com/v1/gifs/search', {params: {q: searchTerm, api_key: '7i1v2G2AELzzTnZPvjNV5I4FKYMP3hA5'}});
-    console.log(result);
-    const gif = document.querySelector('#gif');
-    gif.src = result.data.data[0].images.original.url; //outputs the same GIF when using the same search term; use math random to randomize the index of data
+// remove all GIFS when remove  button is clicked on
+function removeGifsHandler (e) {
+    const gifContainer = document.querySelector('#gif_container');
+    gifContainer.innerHTML = '';
 }
 
 
 gifForm.addEventListener('submit', gifSearchHandler);
+removeButton.addEventListener('click', removeGifsHandler);
